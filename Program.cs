bb7bc2fc686f6d35a61f5e68c033a1f7fd3f38f6
T4kaa155 <ak77.amirkulal@gmail.com>
2026-04-15 20:18:11 +0700

    Polimorfisme memakai method override

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class MetodePembayaran
+{
+    public virtual void Bayar(int jumlah)
+    {
+        Console.WriteLine("Melakukan pembayaran...");
+    }
+}
+
+class BankTransfer : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine($"Silahkan transfer sejumlah Rp {jumlah} ke nomor 67676767");
+    }
+}
+
+class EWallet : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine($"Saldo terpotong sejumlah Rp {jumlah}");
+    }
+}
+
+class KartuKredit : MetodePembayaran
+{
+    public override void Bayar(int jumlah)
+    {
+        Console.WriteLine($"Transaksi sejumlah Rp {jumlah} berhasil dilakukan");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("----Simulasi Checkout----");
+        Console.WriteLine("Pilih metode:");
+        Console.WriteLine("1. Bank Transfer");
+        Console.WriteLine("2. E-Wallet");
+        Console.WriteLine("3. Kartu Kredit");
+
+        Console.Write("Pilihan Anda (1/2/3): ");
+        int pilihan = Convert.ToInt32(Console.ReadLine());
+
+        MetodePembayaran metode;
+
+        switch (pilihan)
+        {
+            case 1:
+                metode = new BankTransfer();
+                break;
+            case 2:
+                metode = new EWallet();
+                break;
+            case 3:
+                metode = new KartuKredit();
+                break;
+            default:
+                Console.WriteLine("Pilihan tidak valid");
+                return;
+        }
+
+        metode.Bayar(100000);
+    }
+}
